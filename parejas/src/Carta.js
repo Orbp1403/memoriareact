d7e39a0f7ebe29e932ed33c130381d7a1a994211
Oscar Rolando Bernard Peralta <2492944860101@ingenieria.usac.edu.gt>
2021-07-07 15:36:59 -0600

    Agregando efecto de volteo a las tarjetas

--- parejas/src/Carta.js
+++ parejas/src/Carta.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
 import './Carta.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAd } from '@fortawesome/free-solid-svg-icons'
+import { FlexyFlipCard } from 'flexy-flipcards'
+import ReactCardFlip from 'react-card-flip'
 
 export default class Carta extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            isFlipped:false
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState(prevState => ({ isFlipped : !prevState.isFlipped }))
+    }
     render() {
         return (
-            <div className="col">
-                <div className="p-3 border bg-warning carta">
-                    <h1 className="icono">
-                        <FontAwesomeIcon icon={this.props.imagen} className="icono"></FontAwesomeIcon>
-                    </h1>
+            <ReactCardFlip isFlipped={this.state.isFlipped}>
+                <div className="col">
+                    <div className="p-3 border bg-warning carta" onClick={this.handleClick}>
+                    </div>
                 </div>
-            </div>
+                <div className="col">
+                    <div className="p-3 border bg-warning carta" onClick={this.handleClick}>
+                        <h1 className="icono">
+                            <FontAwesomeIcon icon={this.props.imagen} className="icono"></FontAwesomeIcon>
+                        </h1>
+                    </div>
+                </div>
+            </ReactCardFlip>
         )
     }
 }
